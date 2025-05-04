a72fdf01132847941a6fc80a2e7448bae986cf52
Imam Mukhri <imammukhri13@gmail.com>
2025-05-04 09:19:34 +0700

    feat: Implement image gallery modal using Bootstrap

---
+++ script.js
@@ -0,0 +1,9 @@
+function showImage(src) {
+    // Temukan elemen img di dalam modal
+    const modalImage = document.getElementById('modalImage');
+    // Setel atribut src dari elemen img modal dengan src gambar yang diklik
+    modalImage.src = src;
+}
+
+// Anda bisa menambahkan kode JS lain di sini nanti
+console.log("Script.js loaded!"); // Pesan untuk cek apakah file JS termuat
